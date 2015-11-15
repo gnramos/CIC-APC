2291cfcfb3331fd22ab2ab768a262da3316b20b8
Guilherme N. Ramos <ramos@gnramos.com>
2015-11-15 21:46:40 -0200

    Ajuste do texto explicativo.

--- src/exercicios/5_EstruturasDeDados/6_Arquivo/0-mencoes.c
+++ src/exercicios/5_EstruturasDeDados/6_Arquivo/0-mencoes.c
@@ -2,8 +2,9 @@
  *     @author: Guilherme N. Ramos (gnramos@unb.br)
  * @disciplina: Algoritmos e Programação de Computadores
  *
- * Faça um programa que leia os nomes e menções de 50 alunos
- * e os escreva em um arquivo binário. */
+ * Faça um programa que leia (da entrada padrão) os nomes e
+ * menções de 50 alunos e os escreva em um arquivo binário
+ * (qualquer). */
 
 #include <stdio.h>
 
