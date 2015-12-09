370fb84c21c5801ac35a50eee9d4f2fe531c315e
Guilherme N. Ramos <ramos@gnramos.com>
2015-12-09 13:37:13 -0200

    Ajuste de documentação

--- src/exercicios/6_DepuracaoTestes/4-intermediario.c
+++ src/exercicios/6_DepuracaoTestes/4-intermediario.c
@@ -2,9 +2,11 @@
  *     @author: Guilherme N. Ramos (gnramos@unb.br)
  * @disciplina: Algoritmos e Programação de Computadores
  *
- * Exemplo de código para depuração. Há alguns problemas, tente encontrá-los
- * com o gdb (e pratique os comandos), e imagine como pode ser difícil 
- * depurar sistemas complexos... */
+ * Exemplo de código para depuração. Não há um comportamento esperado útil, o
+ * foco aqui é explorar o uso da ferramente (gdb). Há alguns problemas, tente
+ * encontrá-los com o depurador (pratique os comandos), e - sabendo que este é
+ * um "sistema fácil" - imagine como pode ser difícil depurar sistemas
+ * complexos... */
 
 #include <stdio.h>
 
