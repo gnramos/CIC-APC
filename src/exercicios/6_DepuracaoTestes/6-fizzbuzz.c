77cf832494fd18bed5bd7dcf731ff26b437ba1d3
Guilherme N. Ramos <ramos@gnramos.com>
2016-04-06 08:02:25 -0300

    Ajuste de enunciado

--- src/exercicios/6_DepuracaoTestes/6-fizzbuzz.c
+++ src/exercicios/6_DepuracaoTestes/6-fizzbuzz.c
@@ -11,8 +11,8 @@
  * 1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz, 16,
  * 17, fizz, 19, buzz, fizz, 22, 23, fizz, buzz, 26, fizz, 28, 29, fizzbuzz
  *
- * Implemente a função FizzBuzz que recebe um número e define o que deve ser
- * dito. Faça testes para tentar garantir que seu código funciona corretamente.
+ * Implemente a função FizzBuzz, que recebe um número e define o que deve ser
+ * dito, usando TDD.
  */
 
 #include <assert.h> /* A macro "assert" testa a condição dada. */
