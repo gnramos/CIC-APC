87c5309f40fb7f73fdc968902a92c800f77f4d7c
gnramos <gnramos@users.noreply.github.com>
2016-10-31 09:36:00 -0200

    Update 04-print.py

--- src/exemplos/01_Interface/00_Saida/04-print.py
+++ src/exemplos/01_Interface/00_Saida/04-print.py
@@ -1,5 +1,5 @@
 #  -*- coding: utf-8 -*-
-#       @file:4-print.py
+#       @file: 04-print.py
 #     @author: Guilherme N. Ramos (gnramos@unb.br)
 # @disciplina: Algoritmos e Programação de Computadores
 #
