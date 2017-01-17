0f1f043dd8bb9d9537ef13510dbad8ab4ae03eb6
Guilherme N. Ramos <ramos@gnramos.com>
2017-01-17 16:53:31 -0200

    Ajuste de formatação.

--- src/exemplos/05_EstruturasDeDados/06_Arquivo/00-binario.py
+++ src/exemplos/05_EstruturasDeDados/06_Arquivo/00-binario.py
@@ -1,5 +1,5 @@
 #  -*- coding: utf-8 -*-
-#      @file: 0-binario.py
+#      @file: 00-binario.py
 #     @author: Gabriel O. Taumaturgo
 # @disciplina: Algoritmos e Programação de Computadores
 #
@@ -17,18 +17,18 @@ def tamanho_string():
 #  * contrário. */
 def cria(arquivo):
 
-    str = bytearray('Algoritmos e Programacao de Computadores'.ljust(tamanho_string(), " "), 'utf8') # preencha com espaços até que tenha 50 caracteres
-    d = bytearray(struct.pack("d", 12.23))
-    i = bytearray(struct.pack("i", 101))
+    str = bytearray('Algoritmos e Programacao de Computadores'.ljust(tamanho_string(), ' '), 'utf8') # preencha com espaços até que tenha 50 caracteres
+    d = bytearray(struct.pack('d', 12.23))
+    i = bytearray(struct.pack('i', 101))
       # O que acontece se não for possível abrir o arquivo? (ele
       # não existe, você não tem permissão para tanto, alguém já
       # o está manipulando, etc.)
-    file = open(arquivo, "wb") # "b" de "binário"...
+    file = open(arquivo, 'wb') # 'b' de 'binário'...
 
     #  É fácil verificar: se for possível abrir o arquivo,
     # o arquivo não está fechado após a abertura.
     if(file.closed):
-        print ("Não foi possível abrir \"%s\".\n" % arquivo)
+        print ('Não foi possível abrir \'%s\'.\n' % arquivo)
         return False
 
     # Escreve no arquivo na ordem: string -> real -> inteiro.
@@ -44,7 +44,7 @@ def cria(arquivo):
 
     # Mostra na saída padrão o conteúdo do arquivo dado como argumento. */
 def mostra(arquivo):
-  file = open(arquivo, "rb")
+  file = open(arquivo, 'rb')
 
   if(file.closed):
       exit(1) # encerre o programa com código de erro 1
@@ -54,39 +54,39 @@ def mostra(arquivo):
   # caso contrário.
 
   str = file.read(tamanho_string()) #  tem de ser do tamanho certo!
-  d = struct.unpack("d",file.read(8))
-  i = struct.unpack("i",file.read(4))
+  d = struct.unpack('d', file.read(8))
+  i = struct.unpack('i', file.read(4))
   file.close()
 
-  print ("string = %s\n" % str)
-  print ("double = %lf\n" % d)
-  print ("int = %d\n" % i)
+  print ('string = %s\n' % str)
+  print ('double = %lf\n' % d)
+  print ('int = %d\n' % i)
 
 
 # Mostra na saída padrão o conteúdo do arquivo dado como argumento, mas lê as
 # informações em ordem diferente da que foi escrita. */
 def mostra_errado(arquivo):
-  file = open(arquivo, "rb");
+  file = open(arquivo, 'rb');
 
   if(file.closed):
       exit(1)
 
   # Lê do arquivo na ordem: inteiro -> real -> string.
-  i = struct.unpack("i",file.read(4))[0]
-  d = struct.unpack("d",file.read(8))[0]
-  str = struct.unpack("50s", file.read(tamanho_string()))[0] #  tem de ser do tamanho certo!
+  i = struct.unpack('i',file.read(4))[0]
+  d = struct.unpack('d',file.read(8))[0]
+  str = struct.unpack('50s', file.read(tamanho_string()))[0] #  tem de ser do tamanho certo!
   file.close()
 
-  print ("string = %s\n" % str)
-  print ("double = %lf\n" % d)
-  print ("int = %d\n" % i)
+  print ('string = %s\n' % str)
+  print ('double = %lf\n' % d)
+  print ('int = %d\n' % i)
 
 
-arquivo = "apc.bin"
+arquivo = 'apc.bin'
 
 if(cria(arquivo)):
-    print ("Leitura correta:\n")
+    print ('Leitura correta:\n')
     mostra(arquivo)
-    print ("\nLeitura incorreta:\n")
+    print ('\nLeitura incorreta:\n')
     mostra_errado(arquivo)
 
