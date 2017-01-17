7f3d1fccd9909dd962de55fa55f9a16466259169
Guilherme N. Ramos <ramos@gnramos.com>
2017-01-17 13:01:31 -0200

    Ajuste de formatação.

--- src/exemplos/05_EstruturasDeDados/05_Registro/00-data.c
+++ src/exemplos/05_EstruturasDeDados/05_Registro/00-data.c
@@ -1,4 +1,4 @@
-/**      @file: registro/0-data.c
+/**      @file: 00-data.c
  *     @author: Guilherme N. Ramos (gnramos@unb.br)
  * @disciplina: Algoritmos e Programação de Computadores
  *
@@ -7,36 +7,37 @@
 #include <stdio.h>
 
 int main() {
-    /* Definição da estrutura do registro (identificado como "data"): */
-    struct {
-        int dia, mes, ano;  /* Armazenam "data" em si. */
-        char descricao[50]; /* Armazenam uma descrição da data. */
-    } data;
+  /* Definição da estrutura do registro (identificado como "data"): */
+  struct {
+    int dia, mes, ano;  /* A "data" em si. */
+    char descricao[50]; /* Uma descrição da data. */
+  } data;
 
-    printf("Digite a descrição: ");
-    scanf("%[^\n]", data.descricao); /* Armazene a informação lida da entrada
-                                        padrão no espaço identificado como
-                                        "descricao" dentro da variável "data"
-                                        (registro). */
+  /* Armazene a informação lida da entrada padrão no espaço identificado como
+    "descricao" dentro da variável "data" (registro). */
+  printf("Digite a descrição: ");
+  scanf("%[^\n]", data.descricao);
 
-    printf("Digite o ano: ");
-    scanf("%d", &(data.ano)); /* Armazene a informação lida da entrada padrão no
-                                 espaço identificado como "ano" dentro da
-                                 variável "data" (registro). */
-    printf("Digite o mês: ");
-    scanf("%d", &(data.mes)); /* Armazene a informação lida da entrada padrão no
-                                 espaço identificado como "mes" dentro da
-                                 variável "data" (registro). */
-    printf("Digite o dia: ");
-    scanf("%d", &(data.dia)); /* Armazene a informação lida da entrada padrão no
-                                 espaço identificado como "dia" dentro da
-                                 variável "data" (registro). */
+  /* Armazene a informação lida da entrada padrão no espaço identificado como
+    "ano" dentro da variável "data" (registro). */
+  printf("Digite o ano: ");
+  scanf("%d", &(data.ano));
 
-    /* Acesse as informações armazenadas e mostre-as. */
-    printf("%s:\n%02d/%02d/%04d\n", data.descricao,
-                                    data.dia,
-                                    data.mes,
-                                    data.ano);
+  /* Armazene a informação lida da entrada padrão no espaço identificado como
+    "mes" dentro da variável "data" (registro). */
+  printf("Digite o mês: ");
+  scanf("%d", &(data.mes));
 
-    return 0;
+  /* Armazene a informação lida da entrada padrão no espaço identificado como
+    "dia" dentro da variável "data" (registro). */
+  printf("Digite o dia: ");
+  scanf("%d", &(data.dia));
+
+  /* Acesse as informações armazenadas e mostre-as. */
+  printf("%s:\n%02d/%02d/%04d\n", data.descricao,
+                                  data.dia,
+                                  data.mes,
+                                  data.ano);
+
+  return 0;
 }
