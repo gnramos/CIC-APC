eee086d0387144118720f55a6a9ab8b3a358adcb
Guilherme N. Ramos <ramos@gnramos.com>
2018-11-21 11:14:28 -0200

    Remoção de arquivo obsoleto.

--- src/exemplos/03_Fluxo/01_Bifurcacao/multipla.py
+++
@@ -1,8 +0,0 @@
-#  -*- coding: utf-8 -*-
-#    @package: multipla.py
-#     @author: Guilherme N. Ramos (gnramos@unb.br)
-# @disciplina: Algoritmos e Programação de Computadores
-#
-# Em Python, não há decisão múltipla para controle de fluxo.
-#
-# Veja https://www.python.org/dev/peps/pep-3103/
