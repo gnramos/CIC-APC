478b1da526cc15b4f5a1c1212a33d5187bdceb82
Guilherme N. Ramos <ramos@gnramos.com>
2020-03-13 16:42:32 -0300

    Ajuste de código.

--- src/exemplos/Projetos/diff.py
+++ src/exemplos/Projetos/diff.py
@@ -19,21 +19,20 @@ def project_dir(project_dir):
 
 
 def parse_args():
-    """Processa os argumentos fornecidos."""
+    u"""Processa os argumentos fornecidos."""
     ap = ArgumentParser(description='Apresenta a evolução do código de um '
                                     'projeto.')
     ap.add_argument('projeto', type=project_dir,
                     help='Nome do projeto cujos arquivos devem ser '
                          'processados.')
-    ap.add_argument('-l', '--linguagem',
+    ap.add_argument('-e', '--extensao',
                     help='Extensão dos arquivos a processar.',
                     default='c', choices=['c', 'py'])
     return ap.parse_args()
 
 
 def terminal(cmd):
-    """Executa o comando dado no terminal."""
-
+    u"""Executa o comando dado no terminal."""
     # ************
     # * ATENÇÃO! *
     # ************
@@ -42,22 +41,21 @@ def terminal(cmd):
     subprocess.run('clear && {}'.format(cmd), shell=True)
 
 
-def diff_files(projeto, linguagem):
+def diff_files(projeto, extensao):
     u"""Mostra a diferença entre arquivos de código do projeto.
 
     Itera pela lista de todos os arquivos de código do projeto, em ordem
     alfabética, e mostra a diferença entre eles com o comando diff.
     """
-    if not projeto.endswith('/'):
-        projeto += '/'
-
-    files = sorted('{}{}'.format(projeto, file)
-                   for file in os.listdir(projeto)
-                   if file.endswith('.' + linguagem))
+    files = []
+    with os.scandir(projeto) as it:
+        files = sorted(entry.path
+                       for entry in it
+                       if entry.is_file() and entry.name.endswith(extensao))
 
     if files:
         terminal('cat {}'.format(files[0]))
-        input('\n [Enter] para continuar...')
+        input('\n [Enter] para continuar (0/{})...'.format(len(files)))
 
         for x in range(1, len(files)):
             terminal('diff {} {}'.format(files[x - 1], files[x]))
@@ -68,7 +66,7 @@ def diff_files(projeto, linguagem):
 
 if __name__ == '__main__':
     args = parse_args()
-    diff_files(args.projeto, args.linguagem)
+    diff_files(args.projeto, args.extensao)
 
 
 # A função "run" (definida em subprocess) é usada no seu programa para
